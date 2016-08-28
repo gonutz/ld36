77d5455ed3bd0c6f7ca5df12a333d79dc3fe2cf2
Lars <lars.scheme@gmail.com>
2016-08-28 12:12:56 +0200

    Disable CGo pointer checks
    Occasionally the check panics at program start. This is undesired and
    might either be a bug in one of the used DirectX libraries or in the
    runtime check itself. It is now disabled and the program runs fine on
    the test systems so it should be OK.

--- build.bat
+++ build.bat
@@ -6,7 +6,10 @@ go get github.com/gonutz/ld36/rsc
 go run make_assets.go ..\bin\blob
 cd ..
 
+setlocal
+set GODEBUG=cgocheck=0
 go build -ldflags -H=windowsgui -o bin\ld36_no_data.exe
+endlocal
 
 cd bin
 payload -exe=ld36_no_data.exe -data=blob -output ld36.exe
